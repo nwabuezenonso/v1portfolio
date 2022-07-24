b4abcdb1ecdec68e2616fc5365b7002acc585481
stephen <nwabuezesc@gmail.com>
2022-07-24 17:42:44 +0100

    adding navigation bar jsx and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Navbar } from './components' 
 
 function App() {
   return (
     <div className="App">
-      <div className=" background">
-        
+      <div className="background">
+        <Navbar/>
       </div>
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+
+        <nav>
+            <div className="app__navbar-logo">
+                CHINONSO
+            </div>
+
+            <div className="hamburger_menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar;
