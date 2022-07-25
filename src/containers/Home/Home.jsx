75ed3e39e38e5a81748302ddee697e01b8e27eb8
stephen <nwabuezesc@gmail.com>
2022-07-25 07:05:08 +0100

    styling the resume info link

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -8,8 +8,10 @@ const Home = () => {
         <p>Hi, i’m <span>Chinonso Nwabueze.</span></p>
         <h1>Software Engineer</h1>
         <p>currently based in Nigeria. I create code based innovation and solve problems using web technologies. I am currently exploring blockchain technologies</p>
-        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 0V3H1L4 6L7 3H5V0H3ZM0 7V8H8V7H0Z" fill="black"/></svg><a>Resume</a><br/>
-        <div></div>
+       <div id='resume'>
+          <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 0V3H1L4 6L7 3H5V0H3ZM0 7V8H8V7H0Z" fill="black"/></svg><a>Resume</a><br/>
+          <div></div>
+       </div>
         <button>About</button>
       </div>
     </div>
