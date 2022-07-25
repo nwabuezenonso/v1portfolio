4c4b7c286f5db36516442f9835bb25bb69578aa3
stephen <nwabuezesc@gmail.com>
2022-07-25 05:52:52 +0100

    adding styles for mobile view

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,7 +3,13 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='intro'>
+      <div className='intro_text'>
+        <p>Hi, i’m Chinonso Nwabueze.</p>
+        <h2>Software Engineer</h2>
+        <p>currently based in Nigeria. I create code based innovation and solve problems using web technologies. I am currently exploring blockchain technologies</p>
+      </div>
+    </div>
   )
 }
 
