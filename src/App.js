0c325acd091004355b4ddc883b886d79e7a2d440
stephen <nwabuezesc@gmail.com>
2022-07-25 05:36:15 +0100

    installing new library and (react-router,creating routes and changing the file directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Navbar,Footer } from './components' 
+import { Routes, Route} from 'react-router-dom';
+import { Navbar,Footer } from './components';
+import { Home } from './containers'
 
 function App() {
   return (
     <div className="App">
       <div className="background">
         <Navbar/>
-        
+        <Routes>
+          <Route path="/" element={<Home /> }/>
+        </Routes>
         <Footer />
       </div>
     </div>
