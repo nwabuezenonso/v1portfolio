e31b643c5352f228ce356501ba571273dc3706ae
stephen <nwabuezesc@gmail.com>
2022-07-25 06:58:08 +0100

    importing a new weight for the font, styling the mobile view of the portfolio website and adding more tags to the web page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,9 +5,12 @@ const Home = () => {
   return (
     <div className='intro'>
       <div className='intro_text'>
-        <p>Hi, i’m Chinonso Nwabueze.</p>
-        <h2>Software Engineer</h2>
+        <p>Hi, i’m <span>Chinonso Nwabueze.</span></p>
+        <h1>Software Engineer</h1>
         <p>currently based in Nigeria. I create code based innovation and solve problems using web technologies. I am currently exploring blockchain technologies</p>
+        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 0V3H1L4 6L7 3H5V0H3ZM0 7V8H8V7H0Z" fill="black"/></svg><a>Resume</a><br/>
+        <div></div>
+        <button>About</button>
       </div>
     </div>
   )
