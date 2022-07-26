44436be041c48af0496f51ff6c88f4080ff56875
stephen <nwabuezesc@gmail.com>
2022-07-26 08:17:14 +0100

    Testing and debugging the home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-// import { Routes, Route} from 'react-router-dom';
-// import { Navbar,Footer } from './components';
-// import { Home } from './containers'
+import { Routes, Route} from 'react-router-dom';
+import { Navbar,Footer } from './components';
+import { Home } from './containers'
 
 function App() {
   return (
     <div className="App">
       <div className="background">
-        {/* <Navbar/> */}
-        {/* <Routes>
+        <Navbar/>
+        <Routes>
           <Route path="/" element={<Home /> }/>
-        </Routes> */}
-        {/* <Footer /> */}
+        </Routes>
+        <Footer />
       </div>
     </div>
   );
