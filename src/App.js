105a238d436d856cc1fbff20f7a3f1672d1480b7
stephen <nwabuezesc@gmail.com>
2022-07-26 07:44:45 +0100

    retry deploy for testing and debugging

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Routes, Route} from 'react-router-dom';
+// import { Routes, Route} from 'react-router-dom';
 import { Navbar,Footer } from './components';
-import { Home } from './containers'
+// import { Home } from './containers'
 
 function App() {
   return (
