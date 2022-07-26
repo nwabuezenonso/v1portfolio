c8f66b27e8ae7cc935321b513faec3e4c44a88f9
stephen <nwabuezesc@gmail.com>
2022-07-26 07:50:18 +0100

    testing and debugging by removing the navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 // import { Routes, Route} from 'react-router-dom';
-import { Navbar,Footer } from './components';
+// import { Navbar,Footer } from './components';
 // import { Home } from './containers'
 
 function App() {
   return (
     <div className="App">
       <div className="background">
-        <Navbar/>
+        {/* <Navbar/> */}
         {/* <Routes>
           <Route path="/" element={<Home /> }/>
         </Routes> */}
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
   );
