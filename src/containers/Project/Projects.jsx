421e8d80adea36debcb7478fa2b63c0d54b181ba
stephen <nwabuezesc@gmail.com>
2022-07-29 16:08:47 +0100

    adding a link to the  contact section

--- src/containers/Project/Projects.jsx
+++ src/containers/Project/Projects.jsx
@@ -24,32 +24,12 @@ const Projects = () => {
             </ul>
             <button>Live preview</button>
           </div>
-          <div className='project1'>
-            <img src= { image } alt=""/>
-            <h1 className='project_title'>Voice powered expense tracker</h1>
-            <p className='project_body'>A simple voice activated budget calculator used to calculate income and expenses.</p>
-            <ul>
-              <li>Reactjs</li>
-              <li>JavaScript</li>
-              <li>Reactjs</li>
-              <li>Reactjs</li>
-              <li>Reactjs</li>
-            </ul>
-            <button>Live preview</button>
-          </div>
-          <div className='project1'>
-            <img src= { image } alt=""/>
-            <h1 className='project_title'>Voice powered expense tracker</h1>
-            <p className='project_body'>A simple voice activated budget calculator used to calculate income and expenses.</p>
-            <ul>
-              <li>Reactjs</li>
-              <li>JavaScript</li>
-              <li>Reactjs</li>
-              <li>Reactjs</li>
-              <li>Reactjs</li>
-            </ul>
-            <button>Live preview</button>
-          </div>
+        </div>
+
+        <div className='need_a_developer'>
+          <h1>Let’s Work Together <span role="img" aria-label="collaborate">🤝</span></h1>
+          <p>I am currently interested to work on projects and would welcome new collaborations. Let's talk!</p>
+          <button className='portfolioButton'>Contact Me</button>
         </div>
       </div>
     </div>
