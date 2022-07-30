9d3790b144a2f1c9811ea69a19b6a83f67a74b5a
stephen <nwabuezesc@gmail.com>
2022-07-30 08:37:16 +0100

    creating new files for the contact section

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           <Route path="/" element={<Home /> }/>
           <Route path="about" element={<About />} />
           <Route path="projects" element={< Projects/>} />
+          <Route path="contact" element={< Projects/>} />
         </Routes>
         <Footer />
     </div>
