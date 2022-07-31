123ffde1baf8df96f10837d99485cd7e3a0f1ace
stephen <nwabuezesc@gmail.com>
2022-07-31 16:25:23 +0100

    adding contact button and debugging

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
 
         <div className='experience'>
           <h1>Experiences</h1>
-          <p>I have developed problem-solving projects in collaboration with developers and clients, ranging from individuals to startups and non-profit organizations. See my <a href='/'>resume</a>  or visit my <a href='/'>LinkedIn profile</a>  for additional information.</p>
+          <p>I have developed problem solving projects in collaboration with developers and clients, ranging from individuals to startups and non-profit organizations. See my <a href='/'>resume</a>  or visit my <a href='/'>LinkedIn profile</a>  for additional information.</p>
         </div>
 
         <button className='portfolioButton'> View previous projects</button>
