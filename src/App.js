103e55fd57caacd8765b953b0dba3c92ef60f0cb
stephen <nwabuezesc@gmail.com>
2022-08-05 13:29:18 +0100

    adding home screen animation

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import { Home, About, Projects, Contact } from './containers'
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home /> }/>
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={< Projects/>} />
-          <Route path="contact" element={< Contact/>} />
-        </Routes>
-        <Footer />
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home /> }/>
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={< Projects/>} />
+        <Route path="contact" element={< Contact/>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
