377c7e4c8e4196fcb0cc74bb6f56e62b68e501b2
stephen <nwabuezesc@gmail.com>
2022-08-06 17:19:30 +0100

    adding slide animation to other pages

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -4,16 +4,16 @@ import './Contact.css';
 const Contact = () => {
   return (
     <div>
-        <div className='contact'>
-            <div className='heading'>
-              <p>Collaborations are welcome</p>
-              <h1>Let's Work Together <span role="img" aria-label="Let's Work Together">🤝</span></h1>
-            </div>
-            <div className='content'>
-              <p>You can reach out to me via Email or any of the social media icon below, you can stay in touch with me on Twitter, Github and Linkedin where I post my works.</p>
-            </div>
-            <button className='portfolioButton'> Contact Me</button>
+      <div className='contact'>
+        <div className='heading'>
+          <p>Collaborations are welcome</p>
+          <h1>Let's Work Together <span role="img" aria-label="Let's Work Together">🤝</span></h1>
         </div>
+        <div className='content'>
+          <p>You can reach out to me via Email or any of the social media icon below, you can stay in touch with me on Twitter, Github and Linkedin where I post my works.</p>
+        </div>
+        <button className='portfolioButton'> Contact Me</button>
+      </div>
     </div>
   )
 }
