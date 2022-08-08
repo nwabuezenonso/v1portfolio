fd070823c10872e9bbcbc9abefdfb2dd27ba9e53
stephen <nwabuezesc@gmail.com>
2022-08-08 07:55:34 +0100

    making transition slow in the all sections that made use of transition

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
           <h1>Let's Work Together <span role="img" aria-label="Let's Work Together">🤝</span></h1>
         </div>
         <div className='content'>
-          <p>You can reach out to me via Email or any of the social media icon below, you can stay in touch with me on Twitter, Github and Linkedin where I post my works.</p>
+          <p>You can reach out to me via Email or any of the social media icon below <br/> you can stay in touch with me on Twitter, Github and Linkedin where I post my works.</p>
         </div>
         <button className='portfolioButton'> Contact Me</button>
       </div>
