91162a0f013c4d4387267b18517d45a8a2344ee9
stephen <nwabuezesc@gmail.com>
2022-08-14 00:38:52 +0100

    added more projects

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,14 @@
 import background_img2 from './background_img2.jpg'
 import image from './image.png';
-import image2 from './image2.png'
-import image3 from './image3.png'
+import image2 from './image2.png';
+import image3 from './image3.png';
+import image4 from './image4.png';
 
 export {
     image,
     image2,
     image3,
+    image4,
     background_img2,
 }
 
--- src/containers/Project/Projects.jsx
+++ src/containers/Project/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import { Link } from 'react-router-dom';
-import { image, image2, image3} from '../../assets';
+import { image, image2, image3, image4} from '../../assets';
 import { ContactDeveloper } from '../../components';
 
 const project_details = [ 
@@ -10,13 +10,17 @@ const project_details = [
     "project_name": "Weather App",
     "project_details": "Installable and lightweight PWA  that  reads weather forecast",
     "project_tags": "ReactJS, CSS, JavaScript",
+    "source": 'https://github.com/nwabuezenonso/weather-app-pwa',
+    "visit": 'https://fascinating-blancmange-fddc43.netlify.app/',
     "id": 1
   },
   {
     "project_image" : image2,
-    "project_name": "An website build for a photography brand",
-    "project_details": "A photography website.",
+    "project_name": "A photography website.",
+    "project_details": "A photography website. build for a photography brand",
     "project_tags": "ReactJS, CSS",
+    "source": 'https://github.com/nwabuezenonso/damanimedia',
+    "visit": 'https://damanimedia.netlify.app/',
     "id": 2
   },
   {
@@ -24,9 +28,19 @@ const project_details = [
     "project_name": "Bugety",
     "project_details": "A voice controlled budget tracker app for tracking income and expenses",
     "project_tags": "ReactJS, CSS,  Speechly", 
+    "source": 'https://github.com/nwabuezenonso/voice-powered-budget-tracker',
+    "visit": 'https://budget-trackerapp.netlify.app/',
     "id": 3
   },
-
+  {
+    "project_image" : image4,
+    "project_name": "Messenger",
+    "project_details": "A messenging platform that allows users to send messages",
+    "project_tags": "ReactJS, CSS,  Node, Socket io", 
+    "source": 'https://github.com/nwabuezenonso/chat-application',
+    "visit": 'https://62664236be3a43461d7f44f9--the-awesome-chukwunonso-site-d6024.netlify.app/',
+    "id": 4
+  },
 
 ]
 
@@ -48,7 +62,7 @@ const Projects = () => {
                 <h1 className='project_title'>{info.project_name}</h1>
                 <p className='project_body'>{info.project_details}</p>
                 <p className='project_tags'>{info.project_tags}</p>
-                <Link to="/about"><button className='project_btn'>Open site</button></Link>
+                <Link to={info.visit}><button className='project_btn'>Open site</button></Link>
               </div>
             </div>
           ))}
