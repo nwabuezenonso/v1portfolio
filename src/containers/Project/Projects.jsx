f84dd4a89c1f5362fd00554cc7b11da1a656895d
stephen <nwabuezesc@gmail.com>
2022-08-14 03:04:08 +0100

    changes the project tags

--- src/containers/Project/Projects.jsx
+++ src/containers/Project/Projects.jsx
@@ -9,7 +9,7 @@ const project_details = [
     "project_image" : image,
     "project_name": "Weather App",
     "project_details": "Installable and lightweight PWA  that  reads weather forecast",
-    "project_tags": "ReactJS, CSS, JavaScript",
+    "project_tags": "ReactJS + CSS + JavaScript",
     "source": 'https://github.com/nwabuezenonso/weather-app-pwa',
     "visit": 'https://fascinating-blancmange-fddc43.netlify.app/',
     "id": 1
@@ -18,7 +18,7 @@ const project_details = [
     "project_image" : image2,
     "project_name": "A photography website.",
     "project_details": "A photography website. build for a photography brand",
-    "project_tags": "ReactJS, CSS",
+    "project_tags": "ReactJS + CSS",
     "source": 'https://github.com/nwabuezenonso/damanimedia',
     "visit": 'https://damanimedia.netlify.app/',
     "id": 2
@@ -27,7 +27,7 @@ const project_details = [
     "project_image" : image3,
     "project_name": "Bugety",
     "project_details": "A voice controlled budget tracker app for tracking income and expenses",
-    "project_tags": "ReactJS, CSS,  Speechly", 
+    "project_tags": "ReactJS + CSS + Speechly", 
     "source": 'https://github.com/nwabuezenonso/voice-powered-budget-tracker',
     "visit": 'https://budget-trackerapp.netlify.app/',
     "id": 3
@@ -36,7 +36,7 @@ const project_details = [
     "project_image" : image4,
     "project_name": "Messenger",
     "project_details": "A messenging platform that allows users to send messages",
-    "project_tags": "ReactJS, CSS,  Node, Socket io", 
+    "project_tags": "ReactJS + CSS + Node + Socket io", 
     "source": 'https://github.com/nwabuezenonso/chat-application',
     "visit": 'https://62664236be3a43461d7f44f9--the-awesome-chukwunonso-site-d6024.netlify.app/',
     "id": 4
